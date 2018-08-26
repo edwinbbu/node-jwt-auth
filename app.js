b87ca4b7e9c0cd77b63843264c5b5228c173a6e9
edwinbbu <edwinbbu@gmail.com>
2018-08-26 19:40:15 +0530

    removed unwanted files

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 require('dotenv-extended').load();
 var express = require('express');
-var path = require('path');
 var parser = require('body-parser')
-var session = require('express-session');
-var expressLayouts = require('express-ejs-layouts');
 var morgan = require('morgan');
 
 var db=require('./config/database.js');
@@ -11,28 +8,18 @@ var db=require('./config/database.js');
 var app = express();
 var port = process.env.port;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(__dirname+'/static'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }))
-app.use(expressLayouts);
 app.use(morgan('dev'));
 
-app.use(session({
-    secret:'cubereum',
-    saveUninitialized: true,
-    resave: true
-}));
-
-var index=require('./routes/index.js');
 var signup = require('./routes/signup.js');
 var login = require('./routes/login.js');
 var details = require('./routes/details.js');
 var edit = require('./routes/edit.js');
 
-app.use('/',index);
+app.get('/',function(req,res){
+    res.send("Cubereum Auth Service");
+})
 app.use('/user/signup',signup);
 app.use('/user/login',login);
 app.use('/user/details',details);
